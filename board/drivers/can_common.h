5e9db78136e7a055d0b3a309dc91668c19bb4ea8
Robbe Derks <robbe.derks@gmail.com>
2024-12-22 14:41:44 +0100

    CH is on bus1 on party raven

--- board/drivers/can_common.h
+++ board/drivers/can_common.h
@@ -162,21 +162,13 @@ void can_set_forwarding(uint8_t from, uint8_t to) {
 
 void ignition_can_hook(CANPacket_t *to_push) {
   int bus = GET_BUS(to_push);
+  int addr = GET_ADDR(to_push);
+  int len = GET_LEN(to_push);
   if (bus == 0) {
-    int addr = GET_ADDR(to_push);
-    int len = GET_LEN(to_push);
-
     // GM exception
     if ((addr == 0x1F1) && (len == 8)) {
       // SystemPowerMode (2=Run, 3=Crank Request)
       ignition_can = (GET_BYTE(to_push, 0) & 0x2U) != 0U;
-      ignition_can_cnt = 0U;
-    }
-
-    // Tesla exception
-    if ((addr == 0x348) && (len == 8)) {
-      // GTW_status
-      ignition_can = (GET_BYTE(to_push, 0) & 0x1U) != 0U;
       ignition_can_cnt = 0U;
     }
 
@@ -186,6 +178,13 @@ void ignition_can_hook(CANPacket_t *to_push) {
       ignition_can_cnt = 0U;
     }
 
+  }
+
+  // Tesla exception
+  if (((bus == 0) || (bus == 1)) && (addr == 0x348) && (len == 8)) {
+    // GTW_status
+    ignition_can = (GET_BYTE(to_push, 0) & 0x1U) != 0U;
+    ignition_can_cnt = 0U;
   }
 }
 
